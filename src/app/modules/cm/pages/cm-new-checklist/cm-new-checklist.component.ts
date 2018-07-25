842591278cc70a7b370187c2f10a36cfc52781d1
Ellpeeaxe <peixuan.lim.2016@smu.edu.sg>
2018-07-26 01:13:27 +0800

    Included skeleton for documents in checklist

--- src/app/modules/cm/pages/cm-new-checklist/cm-new-checklist.component.ts
+++ src/app/modules/cm/pages/cm-new-checklist/cm-new-checklist.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Message } from '../../../../../../node_modules/primeng/components/common/api';
 import { FormBuilder, FormGroup, FormControl, Validators, FormArray } from '../../../../../../node_modules/@angular/forms';
+import {DialogModule} from 'primeng/dialog';
+import {CheckboxModule} from 'primeng/checkbox';
 
 @Component({
     selector: 'cm-new-checklist',
@@ -16,6 +18,7 @@ export class CMNewChecklistComponent implements OnInit {
 
     // UI Component
     newChecklistForm: FormGroup;
+    checked: boolean = true;
 
     constructor(
         private fb: FormBuilder
@@ -30,12 +33,27 @@ export class CMNewChecklistComponent implements OnInit {
     createForm() {
         this.newChecklistForm = this.fb.group({
             checklistName: new FormControl('', [Validators.required]),
+            required_fields: this.fb.array([
+                this.fb.group({
+                    fieldName: new FormControl('', Validators.required),
+                })
+            ]),
             conditions: this.fb.array([
                 this.fb.group({
                     conditionName: new FormControl('', Validators.required),
                     conditionOptions: new FormControl('', Validators.required)
                 })
-            ])
+            ]),
+            documents: this.fb.group({
+                mandatory: this.fb.array([
+                    this.fb.group({
+                        documentName: new FormControl('', Validators.required),
+                        agentCode: new FormControl('', Validators.required),
+                        // signature: this.checked,
+                        remarks: new FormControl('', Validators.required)
+                    })
+                ])
+            })
         });
 
         this.loading = false;
@@ -55,4 +73,37 @@ export class CMNewChecklistComponent implements OnInit {
         let control = <FormArray>this.newChecklistForm.controls.conditions;
         control.removeAt(index);
     }
+
+    addNewField() {
+        let control = <FormArray>this.newChecklistForm.controls.required_fields;
+        control.push(
+            this.fb.group({
+                fieldName: new FormControl('', Validators.required)
+            })
+        )
+    }
+
+    deleteField(index) {
+        let control = <FormArray>this.newChecklistForm.controls.required_fields;
+        control.removeAt(index);
+    }
+
+    mDisplay: boolean = false;
+    
+    showMDialog() {
+            this.mDisplay = true;
+    }
+    
+    addNewMandatory() {
+        let control = <FormArray>this.newChecklistForm.controls.mandatory;
+        control.push(
+            this.fb.group({
+                documentName: new FormControl('', Validators.required),
+                agentCode: new FormControl('', Validators.required),
+                signature: this.checked,
+                remarks: new FormControl('', Validators.required)
+            })
+        )
+    }
+    
 }
