2c7f7599c9e6f0d5ee9e0a7fc3e69aa5cecf37ed
Junxiang <junxiang.ng.2016@smu.edu.sg>
2018-07-27 22:06:14 +0800

    created conditional docs popup

--- src/app/modules/cm/pages/cm-new-checklist/cm-new-checklist.component.ts
+++ src/app/modules/cm/pages/cm-new-checklist/cm-new-checklist.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Message } from 'primeng/components/common/api';
+import { Message, SelectItem } from 'primeng/components/common/api';
 import { FormBuilder, FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
@@ -14,15 +14,19 @@ export class CMNewChecklistComponent implements OnInit {
     loading = false;
     msgs: Message[] = [];
     mDisplay: boolean = false;
+    cDisplay: boolean = false;
+    oDisplay: boolean = false;
 
     // UI Component
     newChecklistForm: FormGroup;
-    tempMandatoryForm: FormGroup;
+    documentsForm: FormGroup;
     documentName = "";
     agentCode = "";
     signature = true;
     remarks = "";
     checklist: any;
+    conditions: SelectItem[];
+    conditionOptions: SelectItem[];
 
     constructor(
         private fb: FormBuilder
@@ -37,7 +41,7 @@ export class CMNewChecklistComponent implements OnInit {
     createForm() {
         this.newChecklistForm = this.fb.group({
             checklistName: new FormControl('', [Validators.required]),
-            required_fields: this.fb.array([
+            requiredFields: this.fb.array([
                 this.fb.group({
                     fieldName: new FormControl('', Validators.required),
                 })
@@ -47,16 +51,32 @@ export class CMNewChecklistComponent implements OnInit {
                     conditionName: new FormControl('', Validators.required),
                     conditionOptions: new FormControl('', Validators.required)
                 })
-            ]),
-            documents: this.fb.group({
-                mandatory: this.fb.array([])
-            })
+            ])
+        });
+
+        this.documentsForm = this.fb.group({
+            mandatory: this.fb.array([]),
+            conditional: this.fb.array([]),
+            optional: this.fb.array([])
         });
 
         this.loading = false;
     }
 
     addNewCondition() {
+        let i = (+this.newChecklistForm.get('conditions')['length'] - 1) + '';
+
+        this.newChecklistForm.get('conditions').get(i).get('conditionName').markAsDirty();
+        this.newChecklistForm.get('conditions').get(i).get('conditionOptions').markAsDirty();
+
+        if (this.newChecklistForm.get('conditions').get(i).get('conditionName').invalid ||
+            this.newChecklistForm.get('conditions').get(i).get('conditionOptions').invalid) {
+            this.msgs.push({
+                severity: 'error', summary: 'Error', detail: 'Please fill in the condition name options before adding another condition'
+            });
+            return;
+        }
+
         let control = <FormArray>this.newChecklistForm.controls.conditions;
         control.push(
             this.fb.group({
@@ -64,7 +84,6 @@ export class CMNewChecklistComponent implements OnInit {
                 conditionOptions: new FormControl('', Validators.required)
             })
         );
-        console.log(this.newChecklistForm);
     }
 
     deleteCondition(index) {
@@ -73,6 +92,17 @@ export class CMNewChecklistComponent implements OnInit {
     }
 
     addNewField() {
+        let i = (+this.newChecklistForm.get('requiredFields')['length'] - 1) + '';
+
+        this.newChecklistForm.get('requiredFields').get(i).get('fieldName').markAsDirty();
+
+        if (this.newChecklistForm.get('requiredFields').get(i).get('fieldName').invalid) {
+            this.msgs.push({
+                severity: 'error', summary: 'Error', detail: 'Please fill in the field name before adding another field'
+            });
+            return;
+        }
+
         let control = <FormArray>this.newChecklistForm.controls.required_fields;
         control.push(
             this.fb.group({
@@ -87,12 +117,13 @@ export class CMNewChecklistComponent implements OnInit {
     }
 
     showMDialog() {
-        let control = <FormArray>this.newChecklistForm.controls.documents.get('mandatory');
+        let control = <FormArray>this.documentsForm.get('mandatory');
         control.push(
             this.fb.group({
                 documentName: new FormControl('', Validators.required),
                 agentCode: new FormControl('', Validators.required),
-                signature: new FormControl(true, Validators.required),
+                signature: new FormControl(true),
+                canWaiver: new FormControl(false),
                 remarks: new FormControl('')
             })
         );
@@ -100,17 +131,19 @@ export class CMNewChecklistComponent implements OnInit {
     }
 
     addNewMandatory() {
-        let i = (+this.newChecklistForm.controls.documents.get('mandatory').get('length') - 1) + '';
+        let i = (+this.documentsForm.get('mandatory').get('length') - 1) + '';
 
-        this.newChecklistForm.controls.documents.get('mandatory').get(i).get('documentName').markAsDirty();
-        this.newChecklistForm.controls.documents.get('mandatory').get(i).get('agentCode').markAsDirty();
-        this.newChecklistForm.controls.documents.get('mandatory').get(i).get('signature').markAsDirty();
-        this.newChecklistForm.controls.documents.get('mandatory').get(i).get('remarks').markAsDirty();
+        this.documentsForm.get('mandatory').get(i).get('documentName').markAsDirty();
+        this.documentsForm.get('mandatory').get(i).get('agentCode').markAsDirty();
+        this.documentsForm.get('mandatory').get(i).get('signature').markAsDirty();
+        this.documentsForm.get('mandatory').get(i).get('canWaiver').markAsDirty();
+        this.documentsForm.get('mandatory').get(i).get('remarks').markAsDirty();
 
-        if (this.newChecklistForm.controls.documents.get('mandatory').get(i).get('documentName').invalid ||
-            this.newChecklistForm.controls.documents.get('mandatory').get(i).get('agentCode').invalid ||
-            this.newChecklistForm.controls.documents.get('mandatory').get(i).get('signature').invalid ||
-            this.newChecklistForm.controls.documents.get('mandatory').get(i).get('remarks').invalid) {
+        if (this.documentsForm.get('mandatory').get(i).get('documentName').invalid ||
+            this.documentsForm.get('mandatory').get(i).get('agentCode').invalid ||
+            this.documentsForm.get('mandatory').get(i).get('signature').invalid ||
+            this.documentsForm.get('mandatory').get(i).get('canWaiver').invalid ||
+            this.documentsForm.get('mandatory').get(i).get('remarks').invalid) {
             this.msgs.push({
                 severity: 'error', summary: 'Error', detail: 'Please correct the invalid fields highlighted'
             });
@@ -118,6 +151,99 @@ export class CMNewChecklistComponent implements OnInit {
         }
 
         this.mDisplay = false;
+    }
+
+    cancelAddNewMandatory() {
+        let i = +this.documentsForm.get('mandatory').get('length') - 1;
+        let control = <FormArray>this.documentsForm.get('mandatory');
+        control.removeAt(i);
+
+        this.mDisplay = false;
+    }
+
+    showCDialog() {
+        let control = <FormArray>this.documentsForm.get('conditional');
+        control.push(
+            this.fb.group({
+                conditions: new FormArray([]),
+                documentName: new FormControl('', Validators.required),
+                agentCode: new FormControl('', Validators.required),
+                signature: new FormControl(true),
+                canWaiver: new FormControl(false),
+                remarks: new FormControl('')
+            })
+        );
+        this.addNewConditionalCondition();
+        this.retrieveConditionalOptions();
+        this.cDisplay = true;
+    }
+
+    retrieveConditionalOptions() {
+        let i: number;
+        this.conditions = [];
+        this.conditionOptions = [];
+        for (i = 0; i < this.newChecklistForm.get('conditions')['length']; i++) {
+            this.conditions.push({
+                "label": this.newChecklistForm.get('conditions').get(i + "").get('conditionName').value,
+                "value": this.newChecklistForm.get('conditions').get(i + "").get('conditionName').value
+            });
+
+            let options = this.newChecklistForm.get('conditions').get(i + "").get('conditionOptions').value.split(',');
+            options.forEach(option => {
+                this.conditionOptions.push({
+                    "label": option.trim(),
+                    "value": option.trim()
+                });
+            });
+        }
+    }
+
+    addNewConditionalCondition() {
+        let i = (+this.documentsForm.get('conditional').get('length') - 1) + "";
+        let control = <FormArray>this.documentsForm.get('conditional').get(i).get('conditions');
+        control.push(
+            this.fb.group({
+                conditionName: new FormControl('', Validators.required),
+                conditionOption: new FormControl('', Validators.required)
+            })
+        );
+    }
+
+    deleteConditionalCondition(index) {
+        let i = (+this.documentsForm.get('conditional').get('length') - 1) + "";
+        let control = <FormArray>this.documentsForm.get('conditional').get(i).get('conditions');
+        control.removeAt(index);
+    }
+
+    addNewConditional() {
+        let i = (+this.documentsForm.get('conditional').get('length') - 1) + '';
+        console.log(this.documentsForm);
+        this.documentsForm.get('conditional').get(i).get('documentName').markAsDirty();
+        this.documentsForm.get('conditional').get(i).get('agentCode').markAsDirty();
+        this.documentsForm.get('conditional').get(i).get('signature').markAsDirty();
+        this.documentsForm.get('conditional').get(i).get('canWaiver').markAsDirty();
+        this.documentsForm.get('conditional').get(i).get('remarks').markAsDirty();
+
+        if (this.documentsForm.get('conditional').get(i).get('documentName').invalid ||
+            this.documentsForm.get('conditional').get(i).get('agentCode').invalid ||
+            this.documentsForm.get('conditional').get(i).get('signature').invalid ||
+            this.documentsForm.get('conditional').get(i).get('canWaiver').invalid ||
+            this.documentsForm.get('conditional').get(i).get('remarks').invalid) {
+            this.msgs.push({
+                severity: 'error', summary: 'Error', detail: 'Please correct the invalid fields highlighted'
+            });
+            return;
+        }
+
+        this.cDisplay = false;
+    }
+
+    cancelAddNewConditional() {
+        let i = +this.documentsForm.get('conditional').get('length') - 1;
+        let control = <FormArray>this.documentsForm.get('conditional');
+        control.removeAt(i);
+
+        this.cDisplay = false;
     }
 
     saveConditions() {
