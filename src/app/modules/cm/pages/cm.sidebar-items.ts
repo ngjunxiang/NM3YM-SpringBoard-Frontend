defead49034a69978efd8addddce0d291256e441
Junxiang <junxiang.ng.2016@smu.edu.sg>
2018-08-21 03:16:13 +0800

    commented out checklist log link

--- src/app/modules/cm/pages/cm.sidebar-items.ts
+++ src/app/modules/cm/pages/cm.sidebar-items.ts
@@ -9,7 +9,7 @@ export const ROUTES: RouteInfo[] = [
         submenu: [
             { path: '/cm/checklist/manage', title: 'Manage Checklists', icon: '', class: '', label: '', labelClass: '', extralink: false, submenu: [] },
             { path: '/cm/checklist/create', title: 'New Checklist', icon: '', class: '', label: '', labelClass: '', extralink: false, submenu: [] },
-            { path: '/cm/checklist/logs', title: 'View Logs', icon: '', class: '', label: '', labelClass: '', extralink: false, submenu: [] }
+            // { path: '/cm/checklist/logs', title: 'View Logs', icon: '', class: '', label: '', labelClass: '', extralink: false, submenu: [] }
         ]
     }
 ];
