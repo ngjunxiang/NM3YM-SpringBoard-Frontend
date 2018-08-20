1b268e161c4d8057065a7075da4e6a2bb8b443a9
Junxiang <junxiang.ng.2016@smu.edu.sg>
2018-08-21 03:16:00 +0800

    edited checklist log page

--- src/app/modules/cm/pages/cm-view-checklist-logs/cm-view-checklist-logs.component.ts
+++ src/app/modules/cm/pages/cm-view-checklist-logs/cm-view-checklist-logs.component.ts
@@ -1,19 +1,72 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SelectItem, Message } from 'primeng/components/common/api';
+import { ChecklistService } from '../../../../core/services/checklist.service';
+
 @Component({
     selector: 'cm-view-checklist-logs',
     templateUrl: './cm-view-checklist-logs.component.html',
     styleUrls: ['./cm-view-checklist-logs.component.scss']
 })
+
 export class CMViewChecklistLogsComponent implements OnInit {
 
     // UI Control
     loading = false;
+    msgs: Message[] = [];
 
-    constructor() { }
+    // UI Components
+    checklistNameDropdownData: SelectItem[];
+    complianceMOCols: any[];
+    complianceCCols: any[];
+    legalMOCols: any[];
+    legalCCols: any[];
+
+    constructor(
+        checklistService: ChecklistService
+    ) { }
 
     ngOnInit() {
+        this.loading = true;
 
+        this.legalMOCols = [
+            { field: 'documentName', header: 'Document Name' },
+            { field: 'agmtCode', header: 'Agmt Code' },
+            { field: 'remarks', header: 'Remarks' },
+            { field: 'signature', header: 'Signature Required' },
+            { field: 'canWaiver', header: 'Can be Waivered' }
+        ];
+
+        this.legalCCols = [
+            { field: 'documentName', header: 'Document Name' },
+            { field: 'conditionName', header: 'Condition Name' },
+            { field: 'conditionOptions', header: 'Condition Options' },
+            { field: 'agmtCode', header: 'Agmt Code' },
+            { field: 'remarks', header: 'Remarks' },
+            { field: 'signature', header: 'Signature Required' },
+            { field: 'canWaiver', header: 'Can be Waivered' }
+        ];
+
+        this.complianceMOCols = [
+            { field: 'documentName', header: 'Document Name' },
+            { field: 'agmtCode', header: 'Agmt Code' },
+            { field: 'remarks', header: 'Remarks' },
+            { field: 'signature', header: 'Signature Required' }
+        ];
+
+        this.complianceCCols = [
+            { field: 'documentName', header: 'Document Name' },
+            { field: 'conditionName', header: 'Condition Name' },
+            { field: 'conditionOptions', header: 'Condition Options' },
+            { field: 'agmtCode', header: 'Agmt Code' },
+            { field: 'remarks', header: 'Remarks' },
+            { field: 'signature', header: 'Signature Required' }
+        ];
+
+        this.retrieveChecklistNames();
     }
 
+    retrieveChecklistNames() {
+
+    }
 }
