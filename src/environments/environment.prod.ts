e69e7b8fee5abd6a86bb110c76f969a4e1ed02a5
Junxiang <junxiang.ng.2016@smu.edu.sg>
2018-09-27 14:15:44 +0800

    changed team aws backend ip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
 
     // Team AWS Server
-    host: 'http://18.136.194.130'
+    host: 'http://52.221.242.156'
 
     // BNP AWS Server
     // host: 'http://54.169.159.209'
