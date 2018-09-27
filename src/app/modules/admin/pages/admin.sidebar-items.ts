77234e085beace1413cc045aea2734a179493845
Junxiang <junxiang.ng.2016@smu.edu.sg>
2018-09-27 14:23:40 +0800

    added user mgmt icon

--- src/app/modules/admin/pages/admin.sidebar-items.ts
+++ src/app/modules/admin/pages/admin.sidebar-items.ts
@@ -5,7 +5,7 @@ export const ROUTES: RouteInfo[] = [
         path: '/admin/panel', title: 'Admin Panel', icon: 'mdi mdi-view-dashboard', class: '', label: '', labelClass: '', extralink: false, submenu: []
     },
     {
-        path: '', title: 'User Management', icon: '', class: 'has-arrow', label: '', labelClass: '', extralink: false,
+        path: '', title: 'User Management', icon: 'mdi mdi-account-edit', class: 'has-arrow', label: '', labelClass: '', extralink: false,
         submenu: [
             { path: '/admin/usrmgmt/create', title: 'Create User Account', icon: '', class: '', label: '', labelClass: '', extralink: false, submenu: [] },
             { path: '/admin/usrmgmt/update', title: 'Update User Account', icon: '', class: '', label: '', labelClass: '', extralink: false, submenu: [] },
